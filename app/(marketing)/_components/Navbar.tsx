fa40447e23c530e828c192876526c0ee0973a0c0
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-05 15:29:21 +0600

    OK

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,9 +1,23 @@
 "use client";
 
 import React from "react";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import { Logo } from "./Logo";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const scrolled = useScrollTop();
+
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+    </div>
+  );
 };
 
 export default Navbar;
