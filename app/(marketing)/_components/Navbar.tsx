f56f098d50afa50421200a2e93584755cb4c2785
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-06 02:25:53 +0600

    Landing page

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React from "react";
-import { useScrollTop } from "@/hooks/use-scroll-top";
+import useScrollTop from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
@@ -11,11 +12,14 @@ const Navbar = () => {
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
       <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2 ">
+        <ModeToggle />
+      </div>
     </div>
   );
 };
