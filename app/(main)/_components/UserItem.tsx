b0d7631d20073f0715f41a75713a9d3c6c88fcfb
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-08 01:17:09 +0600

    Sidebar

--- app/(main)/_components/UserItem.tsx
+++ app/(main)/_components/UserItem.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { SignOutButton, useUser } from '@clerk/clerk-react';
-// import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu';
+
 import { ChevronsLeftRight } from 'lucide-react';
 import Image from 'next/image';
 import { useState } from 'react';
@@ -18,53 +11,6 @@ export const UserItem = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    //     <div
-    //       role="button"
-    //       className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
-    //     >
-    //       <div className="gap-x-2 flex items-center max-w-[150px]">
-    //         <Avater imageSorce={user?.imageUrl} />
-
-    //         <span className="text-start font-medium line-clamp-1">
-    //           {user?.fullName}&apos;s Jotion
-    //         </span>
-    //       </div>
-    //       <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
-    //     </div>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent
-    //     className="w-80"
-    //     align="start"
-    //     alignOffset={11}
-    //     forceMount
-    //   >
-    //     <div className="flex flex-col space-y-4 p-2">
-    //       <p className="text-xs font-medium leading-none text-muted-foreground">
-    //         {user?.emailAddresses[0].emailAddress}
-    //       </p>
-    //       <div className="flex items-center gap-x-2">
-    //         <div className="rounded-md bg-secondary p-1">
-    //           <Avater imageSorce={user?.imageUrl} />
-    //         </div>
-    //         <div className="space-y-1">
-    //           <p className="text-sm line-clamp-1">
-    //             {user?.fullName}&apos;s Motion
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <DropdownMenuSeparator />
-    //     <DropdownMenuItem
-    //       asChild
-    //       className="w-full cursor-pointer text-muted-foreground"
-    //     >
-    //       <SignOutButton>Log out</SignOutButton>
-    //     </DropdownMenuItem>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
-    // New Versition
     <div>
       <div onClick={() => setIsOpen(!isOpen)}>
         <div
@@ -83,6 +29,7 @@ export const UserItem = () => {
       </div>
 
       {/* DropDown 😐 */}
+
       {isOpen && (
         <div className=" bg-[#fff] absolute w-full ">
           <div className="flex flex-col space-y-4 p-2">
