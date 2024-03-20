73574d516df26e2ac33e36ce73f30a6232af2d80
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-03-20 16:03:51 +0600

    What ??

--- convex/auth.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default {
-  providers: [
-    {
-      domain: "https://fitting-lemur-3.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
